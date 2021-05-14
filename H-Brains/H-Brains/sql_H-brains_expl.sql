00598a475e8720777f5520865ee9d58ad4c8149b
DavidAlexSilva <davidams2004@gmail.com>
2021-05-14 10:27:37 +0100

    commit

---
+++ H-Brains/H-Brains/sql_H-brains_expl.sql
@@ -0,0 +1,14 @@
+select * from explicadores
+order by Primeiro_Nome
+insert into explicadores(PessoaId,Primeiro_Nome,Ultimo_Nome,Morada,Cidade)
+values ('913452432','Raul','Venâncio',' Quinta do Outeiro Amadora','Lisboa')
+insert into explicadores(PessoaId,Primeiro_Nome,Ultimo_Nome,Morada,Cidade)
+values ('913734324','Vanessa','Martins','R. Almirante Reis','Lisboa')
+insert into explicadores(PessoaId,Primeiro_Nome,Ultimo_Nome,Morada,Cidade)
+values ('914257321','Paula','Horta','R. Dolores da Conceição Bobadela','Loures')
+insert into explicadores(PessoaId,Primeiro_Nome,Ultimo_Nome,Morada,Cidade)
+values ('913314525','Susana','Sintra','Avenida das Dores ','Loures')
+insert into explicadores(PessoaId,Primeiro_Nome,Ultimo_Nome,Morada,Cidade)
+values ('917342424','Paulo','Sementinha','Rossio','Lisboa')
+insert into explicadores(PessoaId,Primeiro_Nome,Ultimo_Nome,Morada,Cidade)
+values ('919813232','Saúl','Roberto','R.Santa Catarina','Lisboa')
